fb1347e558c98b5b4816808ada69bc7a2df3fbcd
Nils Sommer <mail@nilssommer.de>
2020-01-04 18:00:45 +0100

    Remove boilerplate spec

--- spec/nxt_config_spec.rb
+++ spec/nxt_config_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe NxtConfig do
   it "has a version number" do
     expect(NxtConfig::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
